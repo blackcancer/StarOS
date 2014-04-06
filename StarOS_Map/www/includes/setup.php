2c9309c30ef890c4222df985ca1adca383c15382
Blackcancer <blackcancer@initsysrev.net>
2014-04-06 18:38:13 +0200

    Commenting blueprint line

--- StarOS_Map/www/includes/setup.php
+++ StarOS_Map/www/includes/setup.php
@@ -35,8 +35,8 @@
 	echo "Loading catalog information...\n";
 	createCatalogDatabase($serverDatabase, $SMD);
 
-	echo "Loading blueprint files...\n";
-	createBlueprintDatabase($blueprintsDir, $SMD);
+	//echo "Loading blueprint files...\n";
+	//createBlueprintDatabase($blueprintsDir, $SMD);
 
 	function createEntityDatabase($dir, $SMD) {
 		$entityFiles	= glob($dir . "ENTITY_*", GLOB_NOSORT); // Find all of the playerstate files
